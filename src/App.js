62daba8ac6405d586f5609d0ca962b923ec2d9b5
arunyokesh <ayokesh.cse@gmail.com>
2018-07-26 21:28:32 +0530

    functionalities are done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,81 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			items: [],
+			message: "",
+			result: ""
+		};
+	}
+
+	handleKeyPress(e) {
+		if (e.which === 13) {
+			if (e.target.value != "") {
+				this.setState({
+					items: this.state.items.concat(e.target.value),
+					message: ""
+				});
+				e.target.value = "";
+			} else {
+				this.setState({
+					message: "You Don't have choice"
+				});
+			}
+		}
+	}
+
+	handleLucky() {
+		const { items } = this.state;
+		this.setState({ message: "Finding best one for you..." });
+		const wait = time => new Promise(resolve => setTimeout(resolve, time));
+
+		wait(2000).then(() =>
+			this.setState({
+				result: Math.floor(Math.random() * items.length),
+				message: ""
+			})
+		);
+	}
+
+	render() {
+		const items = this.state.items.map(
+			(item, i) =>
+				i === this.state.result ? (
+					<div key={i} className="form-group-item green">
+						<p>{item}</p>
+					</div>
+				) : (
+					<div key={i} className="form-group-item">
+						<p>{item}</p>
+					</div>
+				)
+		);
+		return (
+			<div className="container">
+				<h1 className="heading">Pick me one!</h1>
+				<div className="form-group">
+					<input
+						className="input"
+						onKeyDown={e => this.handleKeyPress(e)}
+					/>
+					<label className="label">Enter Your Choice</label>
+				</div>
+				<div className="message">{this.state.message}</div>
+				<div className="items">{items}</div>
+				<div className="center">
+					<input
+						className="button"
+						type="button"
+						value="I'm Feeling Lucky!"
+						onClick={() => this.handleLucky()}
+					/>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
