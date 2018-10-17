e1b23abffebd0bcc7fa23b59ccbd2e14777c4ca3
Sunil Naik <sulokit@gmail.com>
2018-10-17 08:35:05 +0530

    Adding remove functionality and correcting CSS

--- src/App.js
+++ src/App.js
@@ -75,12 +75,12 @@ class App extends Component {
 						</div>
 					</div>
 				) : (
-					<div key={i} className="form-group-item">
+					<div key={i} className="form-group-item p-rel">
 						<div>
 							<p>{item}</p>
 						</div>
-						<div>
-						<a className="clear button alert" href="javascript:void(0);" onClick={() => this.handleRemoveItem(i)}>Remove</a>
+						<div className="removeBtnWrapper" onClick={() => this.handleRemoveItem(i)}>
+							<a className="clear button alert no-pad">X</a>
 						</div>
 					</div>
 				)
