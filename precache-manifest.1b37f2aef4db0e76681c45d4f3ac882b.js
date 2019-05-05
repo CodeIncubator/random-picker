ecaa8f4d3182f50a8a641d4f8c4b5366ecd16395
arunyokesh <ayokesh.cse@gmail.com>
2019-05-05 16:16:14 +0530

    Updates

---
+++ precache-manifest.1b37f2aef4db0e76681c45d4f3ac882b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f576a416af10c29fc9d6a48d3493e1fa",
+    "url": "/random-picker/index.html"
+  },
+  {
+    "revision": "7c7258603daee7093d68",
+    "url": "/random-picker/static/css/main.a48bb8f0.chunk.css"
+  },
+  {
+    "revision": "7c31d47951eef567068e",
+    "url": "/random-picker/static/js/2.5c148247.chunk.js"
+  },
+  {
+    "revision": "7c7258603daee7093d68",
+    "url": "/random-picker/static/js/main.48be9416.chunk.js"
+  },
+  {
+    "revision": "a6af48de301d46742859",
+    "url": "/random-picker/static/js/runtime~main.3221dfc1.js"
+  }
+]);
